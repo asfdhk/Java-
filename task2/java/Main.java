d084fa37c092b737a67306c5db15efe2e9e746b8
asfdhk <134499499+asfdhk@users.noreply.github.com>
2023-06-15 22:47:48 +0300

    Delete Main.java

--- task2/java/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        TextSave textSave = new TextSave();
-        textSave.textTransformer = new UpDownTransformer();
-        textSave.saveTextToFile(textSave.textTransformer.transform("hello"));
-    }
-}
